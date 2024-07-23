d7d19e875a0965aefeb61051193178e387624797
AnjaliDebnath <99043926+AnjaliDebnath@users.noreply.github.com>
2024-07-23 09:59:06 +0000

    js

---
+++ FIRST/01-BASICS.js
@@ -0,0 +1,9 @@
+//console.log("hello");
+const aname="Anjali123";
+console.log(aname);
+//console.log(name.substring(0,4));
+//console.log(name.toLowerCase());
+//console.log(aname.split(""));
+console.log(aname.indexOf("l"));
+let arraynum1=[10,2,3,5,7,4];
+console.log(arraynum1.sort());
